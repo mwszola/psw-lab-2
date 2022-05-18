38d2325e1816459d99699816ebb15ad586728026
Mateusz Wszola <matwsz000@utp.edu.pl>
2022-05-18 22:17:33 +0200

    Added solution to Task 1

--- Lab2/Task_1/Program.cs
+++ Lab2/Task_1/Program.cs
@@ -10,14 +10,16 @@ public static class Task1
 {
     public static void Main()
     {
-        var numbers = new int[5];
+        const int numbersLen = 5;
+        var numbers = new int[numbersLen];
         
         Console.WriteLine("Pobieranie wartosci...");
 
-        for (var i = 0; i < 5; i++)
+        for (var i = 0; i < numbersLen; i++)
         {
             Console.WriteLine("Podaj liczbe:");
             var input = Console.ReadLine();
+            // Konwersja danych wejsciowych na liczbe
             var successParse = int.TryParse(input, out var number);
             if (!successParse)
             {
@@ -26,19 +28,23 @@ public static class Task1
 
             numbers[i] = successParse ? number : 0;
         }
+
+        Console.WriteLine("Sortowanie elementow listy...\n");
+        BubbleSort(numbers);
         
-        Console.WriteLine("\n\n");
-        foreach (var number in numbers)
-        {
-            Console.WriteLine(number);
-        }
+        // Formatowanie rezultatu
+        var result = numbers.Aggregate("", (current, number) => current + (number.ToString() + " "));
+        Console.WriteLine("Rezultat: " + result);
+    }
 
-
-        void bubbleSort(IList<int> numbers)
+    private static void BubbleSort(IList<int> numbers)
+    {
+        for (var j = numbers.Count - 1; j > 0; j--)
         {
-            for (var i = 0; i < numbers.Count; i++)
+            for (var i = 0; i < j; i++)
             {
-                
+                if (numbers[i] <= numbers[i + 1]) continue;
+                (numbers[i], numbers[i + 1]) = (numbers[i + 1], numbers[i]);
             }
         }
     }
